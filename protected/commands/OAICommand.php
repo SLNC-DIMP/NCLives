990a17d1670712c0da655aeb774ed252fc3e296a
Dean Farrell <farrelldlfarrell@gmail.com>
2012-05-04 16:46:22 -0400

    Fixed problem in .gitignore Started work on index page. OAI getIdentifers stops when first token encountered, though token is successuflly read. getRecord works correctly.

--- protected/commands/OAICommand.php
+++ protected/commands/OAICommand.php
@@ -14,7 +14,7 @@ class OAICommand extends CConsoleCommand {
 	}
 	
 	protected function updateIdentifier(array $values) {
-		$sql = "UPDATE identifiers SET WHERE id = ?";
+		$sql = "UPDATE identifiers SET url_link = ? WHERE id = ?";
 		Yii::app()->db->createCommand($sql)
 			->execute($values);
 	}
@@ -31,33 +31,42 @@ class OAICommand extends CConsoleCommand {
 	* Checks to see if a OAI resumption token was sent.
 	* If so it is appended to the base url or overwrites the previous token depending on the context
 	* Then makes a call to grab the next batch of identifiers
-	* @param $records
+	* @param $token
 	* @param $url 
 	*/
-	protected function resume(SimpleXMLElement $records, $url) {
-		if(isset($records->resumptionToken)) {
+	protected function resume($token, $url) {
+		if($token) {
 			if(!preg_match('/resumptionToken/i', $url)) {
-				$url = $url . '&resumptionToken=' . $records->resumptionToken;
+				$url = $url . '&resumptionToken=' . $token;
 			} else {
-				$url = preg_replace('/Token=.*$/i', 'Token=' . $records->resumptionToken, $url);
+				$url = preg_replace('/Token=.*$/i', 'Token=' . $token, $url);
 			}
-			$this->getIdentifiers($url);
+			
+			return $url;
 		}
+		
+		return false;
 	}
 	
 	/**
 	* @param $url
 	*/
 	protected function getIdentifiers($url) {
-		$oai_records = simplexml_load_file($url);
-		
-		foreach($oai_records as $records) {
-			foreach($records as $record) {
-				$this->writeIdentifier(array($record->identifier, $record->datestamp));
+		if($url) {
+			$oai_records = simplexml_load_file($url);
+			
+			foreach($oai_records as $records) {
+				foreach($records as $record) {
+					if(!empty($record)) {
+						$this->writeIdentifier(array($record->identifier, $record->datestamp));
+					}
+				} 
+				
 			}
+			$token = ($records->resumptionToken) ? $records->resumptionToken : false;
+			$new_url = $this->resume($token, $url);
+			$this->getIdentifiers($new_url);
 		}
-		
-		$this->resume($records, $url);
 	} 
 	
 	/**
@@ -65,7 +74,7 @@ class OAICommand extends CConsoleCommand {
 	* See http://www.itsalif.info/content/php-5-xmlreader-reading-xml-namespace-part-2
 	* @param $id_string IA identifier for a particular record
 	*/
-	protected function getRecord($id_string) {
+	protected function getRecord($db_id, $id_string) {
 		$request_url = $this->base_oai_url . "verb=GetRecord&metadataPrefix=oai_dc&identifier=$id_string";
 		$xml = new XMLReader();
 		$xml->open($request_url);
@@ -74,7 +83,7 @@ class OAICommand extends CConsoleCommand {
 			if($xml->nodeType == XMLREADER::ELEMENT && $xml->localName === 'identifier') {
 				$xml->read();
 				if(preg_match('/^http/i', $xml->value)) {
-					$xml->value;
+					$this->updateIdentifier(array($xml->value, $db_id));
 				}
 			}
 		}
@@ -82,15 +91,20 @@ class OAICommand extends CConsoleCommand {
 	
 	public function actionIdentifiers() {
 		foreach($this->sets as $set) {
-			$url = $this->base_oai_url . "verb=ListIdentifiers&set=collection:$set&metadataPrefix=oai_dc";
+			$url = $this->base_oai_url . "verb=ListIdentifiers&set=collection:asgii&metadataPrefix=oai_dc";
 			$this->getIdentifiers($url);
 		}		
 	}
 	
+	/**
+	* Thi command won't do anything if there aren't already some identifiers in the db.
+	*/
 	public function actionRecords() {
 		$identifiers = $this->getIdentifiersList();
+		if(empty($identifiers)) { echo "There are no records to retrieve or update\r\n"; exit; }
+		
 		foreach($identifiers as $identity) {
-			$this->getRecords($identity['identifier']);
+			$this->getRecord($identity['id'], $identity['ia_identifier']);
 		}
 	}
 }
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,16 +1,18 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <tt><?php echo __FILE__; ?></tt></li>
-	<li>Layout file: <tt><?php echo $this->getLayoutFile('main'); ?></tt></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<?php $this->beginWidget('bootstrap.widgets.BootHero', array(
+    'heading'=>'Search NCLives!',
+)); ?>
+    <p>(Combining information from the Internet Archives and North Carolina Digital Collections)</p>
+   <?php /** @var BootActiveForm $form */
+	$form = $this->beginWidget('bootstrap.widgets.BootActiveForm', array(
+		'id'=>'searchForm',
+		'type'=>'search',
+		'htmlOptions'=>array('class'=>'well'),
+	)); ?>
+	<?php // echo $form->textFieldRow($model, 'textField', array('class'=>'input-medium')); ?>
+    <input type="text" id="home_search" class="input-xxlarge search-query" placeholder="Search NCLives!" />
+	<?php $this->widget('bootstrap.widgets.BootButton', array('buttonType'=>'submit', 'size'=>'large', 'icon'=>'search', 'label'=>'Search')); ?>
+	 </form>
+	<?php $this->endWidget(); ?>
+<?php $this->endWidget(); ?>
