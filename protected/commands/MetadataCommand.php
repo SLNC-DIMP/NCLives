4cad427ce7f0eee4e11a440fe2bcb3eeb73b3a46
Dean Farrell <farrelldlfarrell@gmail.com>
2012-05-08 16:56:21 -0400

    Downloading of file text for indexing works now.

--- protected/commands/MetadataCommand.php
+++ protected/commands/MetadataCommand.php
@@ -5,7 +5,8 @@ require_once 'File/MARCXML.php';
 
 class MetadataCommand extends CConsoleCommand {
 	protected $marc;
-	protected $base_url = 'http://www.archive.org/download/';
+	protected $meta_url = 'http://www.archive.org/download/';
+	protected $text_url = 'http://archive.org/details/';
 //	protected $metadata;
 	
 	public function __construct() {
@@ -13,12 +14,18 @@ class MetadataCommand extends CConsoleCommand {
 	//	$this->marc = new File_MARCXML($this->base_url . $file . '/' . $file . '_marc.xml', File_MARC::SOURCE_FILE);
 	}
 	
-	protected function getPaths() {
-		$sql = "SELECT id, base_id FROM identifiers WHERE Title IS NULL";
+	protected function getPaths($field) {
+		$sql = "SELECT id, base_id FROM identifiers WHERE $field IS NULL";
 		$query = Yii::app()->db->createCommand($sql)
 			->queryAll();
 		
 		return $query;
+	}
+	
+	protected function writeText(array $fields) {
+		$sql = "UPDATE identifiers SET item_text = ? WHERE id = ?";
+		Yii::app()->db->createCommand($sql)
+			->execute($fields);
 	}
 	
 	protected function writeMetadata(array $fields) {
@@ -42,6 +49,25 @@ class MetadataCommand extends CConsoleCommand {
 	
 	protected function commaReplace($string) {
 		return substr_replace($string, '', -1);
+	}
+	
+	protected function downloadText($identifier) {
+		$url = 'http://archive.org/download/' . $identifier . '/' . $identifier . '_djvu.txt';
+		
+		if($ch = @curl_init($url)) {
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$text = curl_exec($ch);
+		
+		curl_close($ch);
+		} else {
+			echo 'Could not find ' . $url . "\r\n";
+			return false;
+		}
+		
+		return trim($text);
 	}
 	
 	/**
@@ -88,12 +114,27 @@ class MetadataCommand extends CConsoleCommand {
 	}
 	
 	public function actionIaMeta() {
-		$files = $this->getPaths();
+		$files = $this->getPaths('Title');
 		if(empty($files)) { exit; }
 		
 		foreach($files as $file) {
-			$url = $this->base_url . $file['base_id'] . '/' . $file['base_id'] . '_meta.xml';
+			$url = $this->meta_url . $file['base_id'] . '/' . $file['base_id'] . '_meta.xml';
 			$this->getIaMetadata($url, $file['id']);
+		}
+	}
+	
+	public function actionIaText() {
+		$texts = $this->getPaths('item_text');
+		if(empty($texts)) { exit; }
+		
+		foreach($texts as $text) {
+			$full_text = $this->downloadText($text['base_id']);
+			if($full_text) {
+				$this->writeText(array($full_text, $text['id']));
+				echo "Text file for: " . $text['id'] . " downloaded\r\n";
+			} else {
+				echo "Couldn't get text file for: " . $text['id'] . "\r\n";
+			}
 		}
 	}
 }
